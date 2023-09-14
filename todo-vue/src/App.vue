343e9f2ff1e23570b7a6efa01eeba6b6ff0a9614
dragondashing <3296367480@qq.com>
2023-09-14 15:57:29 +0800

    vue框架实践

--- todo-vue/src/App.vue
+++ todo-vue/src/App.vue
@@ -2,23 +2,37 @@
   <div id="app">
     <h1>我的任务清单</h1>
     <ul>
-      <li>
-        <to-do-item label="我的任务清单"></to-do-item>
+      <li v-for="item in ToDoItems" :key="item.id">
+        <to-do-item
+          :label="item.label"
+          :done="item.done"
+          :id="item.id"></to-do-item>
       </li>
     </ul>
   </div>
 </template>
 
+
 <script>
 import ToDoItem from "./components/ToDoItem.vue";
+import uniqueId from "lodash.uniqueid";
 
 export default {
-  name: "app",
-  components: {
-    ToDoItem,
-  },
-};
-
+        name: "app",
+        components: {
+          ToDoItem,
+        },
+        data() {
+          return {
+            ToDoItems: [
+              { id: uniqueId("todo-"), label: "学习Vue", done: false },
+              { id: uniqueId("todo-"), label: "使用CLI创建Vue项目",done:true},
+              { id: uniqueId("todo-"), label: "放松", done: true },
+              { id: uniqueId("todo-"), label: "创建一个任务清单", done: false },
+            ],
+          };
+        },
+      };
 
 </script>
 
